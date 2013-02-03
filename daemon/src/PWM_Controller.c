3fcf03d35fa30860589add762b57713e1949c698
Thomas Burns <ThomasBurns88@gmail.com>
2013-02-03 16:35:47 +1100

    Cleaned up git tracked files. Started building high level framework for Daemon.
    	modified:   ../.gitignore
    	deleted:    Makefile
    	deleted:    Makefile.in
    	modified:   aclocal.m4
    	deleted:    autom4te.cache/output.0
    	deleted:    autom4te.cache/output.1
    	deleted:    autom4te.cache/output.2
    	deleted:    autom4te.cache/requests
    	deleted:    autom4te.cache/traces.0
    	deleted:    autom4te.cache/traces.1
    	deleted:    autom4te.cache/traces.2
    	modified:   config.h
    	deleted:    config.h.in
    	modified:   config.log
    	modified:   config.status
    	modified:   configure
    	deleted:    src/.deps/CmdParse.Po
    	deleted:    src/.deps/PWM_Controller.Po
    	deleted:    src/.deps/Serial.Po
    	new file:   src/Command_List.c
    	new file:   src/Command_List.h
    	modified:   src/Makefile
    	modified:   src/Makefile.am
    	modified:   src/Makefile.in
    	modified:   src/PWM_Controller
    	modified:   src/PWM_Controller.c
    	new file:   src/PWM_Controller.conf
    	modified:   src/PWM_Controller.h
    	new file:   src/PWM_Test
    	new file:   src/parson.c
    	new file:   src/parson.h

--- daemon/src/PWM_Controller.c
+++ daemon/src/PWM_Controller.c
@@ -1,4 +1,5 @@
 // *****************
+#include <fcntl.h>
 #include <signal.h>
 #include <syslog.h>
 #include <stdio.h>
@@ -8,20 +9,53 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
 
+#include "parson.h"
 #include "PWM_Controller.h"
+#include "Serial.h"
 
 // *****************
+const char PWM_Con_Settings_file[] = "PWM_Controller.conf";
+#define SHM_SIZE	sizeof(Pwm_Con_Mem)
+
 Pwm_Con_Mem *PWM_ptr;
+JSON_Value *JSON_Settings;
+JSON_Object *J_Object;
+
+int Serial_fd;
+int Send_data;
+
+// *****************
+void System_Shutdown(int rv)
+{
+  // detach from the segment:
+  if (shmdt(PWM_ptr) == -1)
+  {
+    perror("shmdt");
+    exit(1);
+  }
+  closelog();
+  exit(rv);
+}
 
 // *****************
 int Create_Shared_Memory( void )
 {
   key_t key;
   int shmid;
+	int fd;
 
-  // make the key:
-  if ((key = ftok(PWM_SHARDED_MEM_NAME, PWM_SHARDED_MEM_KEY)) == -1)
+  // Make sure the file exists.
+  fd = open(PWM_KEY_FILE, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+	/* Only wanted to make sure that the file exists. */
+  close(fd);
+
+  // Generate memory key. */
+  key = ftok(PWM_KEY_FILE, PWM_MEM_KEY);
+  if (key  == -1)
   {
     perror("ftok");
     exit(1);
@@ -41,29 +75,76 @@ int Create_Shared_Memory( void )
     perror("shmat");
     exit(1);
   }
+
+  pthread_mutex_init(&PWM_ptr->access, NULL);
   return 0;
 }
 
 // *****************
-void shutdown(int rv)
+void Read_Settings(void)
 {
-  // detach from the segment:
-  if (shmdt(data) == -1)
-  {
-    perror("shmdt");
-    exit(1);
+	int rv;
+
+  JSON_Settings = json_parse_file(PWM_Con_Settings_file);
+	rv = json_value_get_type(JSON_Settings);
+
+  if ( rv != JSONObject )
+	{
+		printf("System didn't work, %d\n", rv );
+    System_Shutdown( -1);
   }
-  closelog();
-  exit(rv);
+
+  J_Object = json_value_get_object(JSON_Settings);
+	if ( J_Object == NULL )
+	{
+		printf("JSON_Object == NULL\n");
+		System_Shutdown( -1);
+	}
+	printf("Opening Serial Port: %s\n", json_object_get_string(J_Object, "Serial_Port"));
 }
 
 // *****************
+void Write_SystemTime(unsigned long time)
+{
+	// write the existing time to the controller.
+}
+
+void Send_PWMChanelData(void)
+{
+}
+
+// *****************
+void Connect_To_Port(void)
+{
+	const char *name = json_object_get_string(J_Object, "Serial_Port");
+
+	if ( name == NULL )
+	{
+		syslog(LOG_NOTICE, "Com Port name not set");
+		System_Shutdown(-1);
+	}
+
+ 	Serial_fd = Serial_Openport((char *)name, 115200, 0,0 );
+	if ( Serial_fd < 0 )
+		return;
+	syslog(LOG_NOTICE, "Com Port %s connected", name);
+	// send start up commands.
+	// read time
+	// send temp limit,
+	// send temp update rate
+	// send current limit,
+	// send current update rate
+	// force unit to restart
+
+}
 
 // *****************
 int main(int argc, char *argv[])
 {
+	int write_time = 0;
+	struct timeval tv;
   int loop = 1;
-  openlog("PWM_Controller", LOG_PID | LOG_NDELY , LOG_USER );
+  openlog("PWM_Controller", LOG_PID , LOG_USER );
   syslog(LOG_NOTICE, "PWM_Controller Startup");
 
 //  printf("become a daemon\n");
@@ -73,21 +154,45 @@ int main(int argc, char *argv[])
   if ( Create_Shared_Memory() < 0 )
   {
     printf("Shared Memory failed\n");
-    shutdown(-1);
+    System_Shutdown(-1);
   }
 
   printf("Read Settings\n");
-  printf("Open Serial Port\n");
-  printf("Send Startup commands\n");
-  sleep(5);
-  printf("\n");
+	Read_Settings();
 
+	while ( loop > 0 )
+	{
+		printf("Open Serial port\n");
 
-  // main loop
-  while ( loop > 0 )
-  {
-  }
-  shutdown(0);
+		Connect_To_Port();
+		while ( Serial_fd >= 0 )
+		{
+			PWM_ptr->port_connected = 1;
+			gettimeofday(&tv, NULL);
+			// run receiver
+			//
+			if (( tv.tv_sec - write_time ) > 3600)
+			{// here once an hour
+				write_time = tv.tv_sec;
+				Write_SystemTime(tv.tv_sec);
+			}
+			else
+			if ( Send_data )
+			{
+				pthread_mutex_lock( &PWM_ptr->access );
+				if ( PWM_ptr->data_ready )
+				{
+					Send_PWMChanelData();
+					PWM_ptr->data_ready = 0;
+				}
+				pthread_mutex_unlock( &PWM_ptr->access );
+				Send_data = 0;
+			}
+		}
+		sleep(10);
+	}
+
+	System_Shutdown(0);
   return 0;
 }
 
--- daemon/src/PWM_Controller.h
+++ daemon/src/PWM_Controller.h
@@ -6,34 +6,32 @@
 #ifndef __PWM_CONTROLLER_H__
 #define __PWM_CONTROLLER_H__
 // *****************
-/*
 #ifdef __cplusplus
 extern "C" {
 #endif
-*/
 // *****************
 #include <pthread.h>
 
-#define PWM_SHARDED_MEM_NAME "/tmp/PWM_Controller"
-#define PWM_SHARDED_MEM_KEY 'P'
+#define PWM_KEY_FILE	"/tmp/PWM_Controller.shmem"
+#define PWM_MEM_KEY 'R'
+
 #define PWM_NUM_CHANELS	16
 #define PWM_CON_SHM_SIZE	1024
 
 typedef struct
 {
-	pmutex_t access;
-	int data_ready;
+	pthread_mutex_t access;
+	unsigned data_ready:1;
+	unsigned port_connected:1;
 	float ch[PWM_NUM_CHANELS];
 	float current;
 	float temperature;
-	char firmware[128]
+	char firmware[128];
 } Pwm_Con_Mem;
 
 // *****************
-/*
 #ifdef __cplusplus
 }
 #endif
-*/
 #endif
 
