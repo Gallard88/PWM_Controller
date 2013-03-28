ee18692246e99811ec800d5146e28d2051e9dcd6
Thomas Burns <ThomasBurns88@gmail.com>
2013-03-28 15:34:28 +1100

    Added new files to git

---
+++ daemon/src/PWM_Lib.c
@@ -0,0 +1,131 @@
+// *****************
+#define _XOPEN_SOURCE
+
+// *****************
+#include <fcntl.h>
+#include <signal.h>
+#include <syslog.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
+#include <errno.h>
+
+// *****************
+#include "ShdMem.h"
+#include "PWM_Controller.h"
+
+// *****************
+Pwm_Shd_Mem *PWM_ptr;
+
+// *****************
+int PWM_Connect(void)
+{
+  key_t key;
+  int shmid;
+  int fd;
+
+  // Make sure the file exists.
+  fd = open(PWM_KEY_FILE, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+  /* Only wanted to make sure that the file exists. */
+  close(fd);
+
+  // Generate memory key. */
+  key = ftok(PWM_KEY_FILE, PWM_MEM_KEY);
+  if (key  == -1)
+  {
+    perror("ftok");
+		return -1;
+  }
+
+  // connect to (and possibly create) the segment:
+  if ((shmid = shmget(key, PWM_CON_SHM_SIZE, O_RDWR)) == -1)
+  {
+    perror("shmget");
+		return -1;
+  }
+
+  // attach to the segment to get a pointer to it:
+  PWM_ptr = shmat(shmid, (void *)0, 0);
+  if ((char *)PWM_ptr == (char *)(-1))
+  {
+    perror("shmat");
+    return -1;
+  }
+  return 0;
+}
+
+// *****************
+int PWM_isConnected(void)
+{
+	int con;
+  pthread_mutex_lock( &PWM_ptr->access );
+  con = PWM_ptr->data_ready;
+  pthread_mutex_unlock( &PWM_ptr->access );
+
+	return con;
+}
+
+// *****************
+float PWM_GetTemp(void)
+{
+	float value;
+
+  pthread_mutex_lock( &PWM_ptr->access );
+  value = PWM_ptr->temperature;
+  pthread_mutex_unlock( &PWM_ptr->access );
+
+	return value;
+}
+
+// *****************
+float PWM_GetCurrent(void)
+{
+	float value;
+
+  pthread_mutex_lock( &PWM_ptr->access );
+  value = PWM_ptr->current;
+  pthread_mutex_unlock( &PWM_ptr->access );
+
+	return value;
+}
+
+// *****************
+float PWM_GetVoltage(void)
+{
+	float value;
+
+  pthread_mutex_lock( &PWM_ptr->access );
+  value = PWM_ptr->voltage;
+  pthread_mutex_unlock( &PWM_ptr->access );
+
+	return value;
+}
+
+// *****************
+void PWM_SetPWM(int ch, float duty)
+{
+	if ( ch >= PWM_NUM_CHANELS )
+		return;
+
+  pthread_mutex_lock( &PWM_ptr->access );
+	if ( duty > 1.0 )
+		duty = 1.0;
+	if ( duty < 0.0 )
+		duty = 0.0;
+
+	PWM_ptr->ch[ch].duty = duty;
+  PWM_ptr->ch[ch].update = time(NULL);
+  PWM_ptr->data_ready = 1;
+
+	pthread_mutex_unlock( &PWM_ptr->access );
+}
+
+// *****************
+// *****************
---
+++ daemon/src/ShdMem.h
@@ -0,0 +1,48 @@
+/*
+
+
+*/
+// *****************
+#ifndef __SHARD_MEMORY_H__
+#define __SHARD_MEMORY_H__
+// *****************
+#ifdef __cplusplus
+extern "C" {
+#endif
+// *****************
+#include <pthread.h>
+#include <time.h>
+
+#define PWM_NUM_CHANELS			16
+#define PWM_FIRMWARE_SIZE		128
+  typedef struct
+  {
+		float duty;
+		time_t update;
+	} PWM_Ch;
+
+  typedef struct
+  {
+    pthread_mutex_t access;
+    unsigned data_ready:1;
+    unsigned port_connected:1;
+    float voltage;
+    float current;
+    float temperature;
+    char firmware[PWM_FIRMWARE_SIZE];
+    PWM_Ch ch[PWM_NUM_CHANELS];
+  } Pwm_Shd_Mem;
+
+//  extern Pwm_Shd_Mem *PWM_ptr;
+
+// *****************
+#define PWM_CON_SHM_SIZE		sizeof(Pwm_Shd_Mem)
+#define PWM_KEY_FILE	"/tmp/PWM_Controller.shmem"
+#define PWM_MEM_KEY 'R'
+
+// *****************
+#ifdef __cplusplus
+}
+#endif
+#endif
+
